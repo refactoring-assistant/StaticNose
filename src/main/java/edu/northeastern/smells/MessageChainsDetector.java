bece7ff0889c627a1da1418040403a272d334ac3
Yash Burshe <bursheyash2@gmail.com>
2026-03-10 15:08:26 -0400

    Add message chains detector

---
+++ src/main/java/edu/northeastern/smells/MessageChainsDetector.java
@@ -0,0 +1,115 @@
+package edu.northeastern.smells;
+
+import edu.northeastern.reporting.ReportStruct;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageChainsDetector extends AbstractDetector {
+
+    // A chain of 3 method calls (a().b().c()) is our threshold for a smell.
+    private static final int CHAIN_THRESHOLD = 3;
+
+    public MessageChainsDetector(List<String> javaFilePaths, String inputDirPath) {
+        super(javaFilePaths, inputDirPath);
+    }
+
+    @Override
+    protected String getSmellName() {
+        return "Message Chain (Law of Demeter Violation)";
+    }
+
+    @Override
+    protected List<Integer> analyzeType(CtType<?> type) {
+        List<Integer> detectedLines = new ArrayList<>();
+
+        List<CtInvocation<?>> invocations = type.getElements(new TypeFilter<>(CtInvocation.class));
+
+        for (CtInvocation<?> inv : invocations) {
+
+            if (isInnerInvocation(inv)) {
+                continue;
+            }
+
+            int chainDepth = getChainDepth(inv);
+
+            if (chainDepth >= CHAIN_THRESHOLD) {
+                if (!isFluentApi(inv)) {
+                    if (inv.getPosition().isValidPosition()) {
+                        detectedLines.add(inv.getPosition().getLine());
+                    }
+                }
+            }
+        }
+
+        return detectedLines;
+    }
+
+    /**
+     * Determines if this invocation is the target of another invocation.
+     * If true, it means we are in the middle of a chain.
+     */
+    private boolean isInnerInvocation(CtInvocation<?> inv) {
+        CtElement parent = inv.getParent();
+        if (parent instanceof CtInvocation) {
+            CtInvocation<?> parentInv = (CtInvocation<?>) parent;
+            return parentInv.getTarget() == inv;
+        }
+        return false;
+    }
+
+    /**
+     * Recursively traces the targets backward to count the number of chained calls.
+     */
+    private int getChainDepth(CtInvocation<?> outermostInv) {
+        int depth = 1;
+        CtExpression<?> currentTarget = outermostInv.getTarget();
+
+        while (currentTarget instanceof CtInvocation) {
+            depth++;
+            currentTarget = ((CtInvocation<?>) currentTarget).getTarget();
+        }
+
+        return depth;
+    }
+
+    /**
+     * Heuristics to identify Builder Patterns, Java Streams, and StringBuilders.
+     */
+    private boolean isFluentApi(CtInvocation<?> outermostInv) {
+        CtExpression<?> currentTarget = outermostInv.getTarget();
+        CtInvocation<?> currentInv = outermostInv;
+
+        while (currentTarget instanceof CtInvocation) {
+            CtInvocation<?> targetInv = (CtInvocation<?>) currentTarget;
+
+            CtTypeReference<?> currentType = currentInv.getType();
+            CtTypeReference<?> targetType = targetInv.getType();
+
+            if (currentType != null && targetType != null && currentType.equals(targetType)) {
+                return true;
+            }
+
+            if (targetType != null) {
+                String typeName = targetType.getQualifiedName();
+                if (typeName.startsWith("java.util.stream") ||
+                        typeName.equals("java.lang.StringBuilder") ||
+                        typeName.equals("java.lang.StringBuffer") ||
+                        typeName.startsWith("java.util.Optional")) {
+                    return true;
+                }
+            }
+
+            currentInv = targetInv;
+            currentTarget = targetInv.getTarget();
+        }
+
+        return false;
+    }
+}
