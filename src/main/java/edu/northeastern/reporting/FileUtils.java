bf83f8017ffe28802590273628455ca0e915330b
Yash Burshe <bursheyash2@gmail.com>
2026-03-21 14:48:59 -0400

    Fix file creation

--- src/main/java/edu/northeastern/reporting/FileUtils.java
+++ src/main/java/edu/northeastern/reporting/FileUtils.java
@@ -7,10 +7,12 @@ public class FileUtils {
     public static File safeCreateDir(String dirPath) {
         File dir = new File(dirPath);
 
-        if(!dir.exists()){
-            dir.mkdir();
+        if (!dir.exists()) {
+            boolean success = dir.mkdirs();
+            if (!success) {
+                throw new RuntimeException("Failed to create directory: " + dirPath);
+            }
         }
-
         return dir;
     }
 
